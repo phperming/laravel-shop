47eb1e91e8f58d920bd3716b8ebe5c36f5774742
phperming <875617373@qq.com>
2019-07-24 08:10:22 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
- function test_helper()
- {
- 	return "ok";
- }
+ function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','首页')
+
+@section('content')
+	<h1>this is first</h1>
+@stop
+
