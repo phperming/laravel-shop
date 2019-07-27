c6a65a1301958188ff3cc91e7306e7d5a585d600
phperming <875617373@qq.com>
2019-07-27 09:39:56 +0000

    用户界面-商品收藏

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}','UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}','UserAddressesController@destory')->name('user_addresses.destory');
+    Route::post('products/{product}/favorite','ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite','ProductsController@disfavor')->name('products.disfavor');
 });
 
