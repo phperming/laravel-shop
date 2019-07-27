f3fa6688555278dad5849d09277fd71432d71f15
phperming <875617373@qq.com>
2019-07-27 10:04:57 +0000

    用户界面-收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -90,4 +90,11 @@ class ProductsController extends Controller
     	$user->favoriteProducts()->detach($product);
     	return [];
     }
+
+    public function favorites(Request $request)
+    {
+    	$products = $request->user()->favoriteProducts()->paginate(16);
+
+    	return view('products.favorites',['products'=>$products]);
+    }
 }
