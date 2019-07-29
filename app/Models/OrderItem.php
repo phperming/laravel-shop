bd6dbafba9085c00b6b25fec27d7fb6c87e8ea47
phperming <875617373@qq.com>
2019-07-29 09:32:23 +0000

    订单模型

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = ['amount','price','rating','review','reviewed_at'];
+
+    protected $dates = ['reviewed_at'];
+    public $timestamp =false;
+
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
+
+    public function productSku()
+    {
+    	return $this->belongsTo(productSku::class);
+    }
+
+    public function order()
+    {
+    	return $this->belongsTo(Order::class);
+    }
+}
---
+++ database/factories/ModelsOrderItemFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\ModelsOrderItem;
+use Faker\Generator as Faker;
+
+$factory->define(ModelsOrderItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
