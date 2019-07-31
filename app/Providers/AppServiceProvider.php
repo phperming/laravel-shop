feb6560e726511e574476639771a28a6c64aa74f
phperming <875617373@qq.com>
2019-07-31 07:41:24 +0000

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,32 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //往服务容器中注入名为 alipay 的一个单例对象
+        $this->app->singleton('alipay',function(){
+            $config = config('pay.alipay');
+            //判断当前环境是否为线上环境
+            if(app()->environment() !== 'product'){
+                $config['mod']  ='dev';
+                $config['log']['level'] = Logger::DEBUG;
+            }else{
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            //调用Yansongda/pay来创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay',function(){
+            if(app()->environment() !== 'product'){
+                $config['log']['level'] = Logger::DEBUG;
+            }else{
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            return Pay::wechat($config);
+        });
+
+        
     }
 
     /**
