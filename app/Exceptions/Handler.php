d118c9a0171dcc3871e017710667a1975c1d4d33
phperming <875617373@qq.com>
2019-08-16 09:00:09 +0000

    用户界面-使用优惠券下单

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -14,6 +14,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         InvalideRequestException::class,
+        CouponCodeUnavailableException::class,
     ];
 
     /**
--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\CouponCode;
+use App\Exceptions\CouponCodeUnavailableException; 
 
 class CouponCodesController extends Controller
 {
@@ -12,25 +13,10 @@ class CouponCodesController extends Controller
     {
     	//判断优惠券是否存在
     	if (!$record = CouponCode::where('code',$code)->first()){
-    		abort(404);
+    		throw new CouponCodeUnavailableException('该优惠券不存在');
     	}
 
-    	//如果优惠券没有启用，等同于不存在
-    	if (!CouponCode::where($record->enabled)) {
-    		abort(404);
-    	}
-
-    	if ($record->total - $record->used <=0 ) {
-    		return response()->json(['该优惠券已经用完'],403);
-    	}
-
-    	if ($record->not_before && $record->not_before->gt(Carbon::now())) {
-    		return response()->json(['msg'=>'该优惠券还不能使用'],403);
-    	}
-
-    	if ($record->not_after && $record->not_after->lt(Carbon::now())) {
-    		return json(['msg'=>'该优惠券已经过期'],403);
-    	}
+    	$record->checkAvailable();
 
     	return $record;
     }
